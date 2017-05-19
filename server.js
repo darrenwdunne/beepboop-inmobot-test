fe0a895650c0b4d2a3e30b64c6585877c35310ec
Darren Dunne <darrenwdunne@gmail.com>
2017-05-19 13:17:47 -0400

    reduce vertical size (fewer fields)

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
 
 // return a random Spark low priority bug from the backlog
 slapp.message('rand', ['mention', 'direct_message'], (msg) => {
-  const bugs = ['VNOW-6343','VNOW-6340','VNOW-6338','VNOW-6334','VNOW-6330','VNOW-6329','VNOW-6328','VNOW-6327','VNOW-6317','VNOW-6215','VNOW-6188','VNOW-6185','VNOW-6184','VNOW-6176','VNOW-6175','VNOW-6172','VNOW-6151','VNOW-6150','VNOW-6146','VNOW-6144','VNOW-6137','VNOW-6133','VNOW-6128','VNOW-6124','VNOW-6122','VNOW-6115','VNOW-6113','VNOW-6107','VNOW-6096','VNOW-6094','VNOW-6093','VNOW-6092','VNOW-6087','VNOW-6082','VNOW-6081','VNOW-6078','VNOW-6076','VNOW-6074','VNOW-6072','VNOW-6042','VNOW-6024','VNOW-6020','VNOW-5992','VNOW-5974','VNOW-5971','VNOW-5961','VNOW-5932','VNOW-5928','VNOW-5914','VNOW-5900','VNOW-5891','VNOW-5890','VNOW-5879','VNOW-5868','VNOW-5867','VNOW-5843','VNOW-5825','VNOW-5804','VNOW-5756','VNOW-5739','VNOW-5733','VNOW-5696','VNOW-5691','VNOW-5643','VNOW-5561','VNOW-5527','VNOW-5492','VNOW-5483','VNOW-5476','VNOW-5452','VNOW-5423','VNOW-5422','VNOW-5399','VNOW-5366','VNOW-5365','VNOW-5340','VNOW-5319','VNOW-5303','VNOW-5293','VNOW-5290','VNOW-5239','VNOW-5224','VNOW-5200','VNOW-5184','VNOW-5160','VNOW-5108','VNOW-5104','VNOW-5072','VNOW-5016','VNOW-4993','VNOW-4989','VNOW-4976','VNOW-4905','VNOW-4903','VNOW-4901','VNOW-4900','VNOW-4874','VNOW-4873','VNOW-4870','VNOW-4864','VNOW-4840','VNOW-4835','VNOW-4834','VNOW-4830','VNOW-4817','VNOW-4809','VNOW-4808','VNOW-4807','VNOW-4806','VNOW-4758','VNOW-4742','VNOW-4725','VNOW-4707','VNOW-4577','VNOW-4547','VNOW-4250','VNOW-3870','VNOW-3759','VNOW-3523','VNOW-2903']
+  const bugs = ['VNOW-6343', 'VNOW-6340', 'VNOW-6338', 'VNOW-6334', 'VNOW-6330', 'VNOW-6329', 'VNOW-6328', 'VNOW-6327', 'VNOW-6317', 'VNOW-6215', 'VNOW-6188', 'VNOW-6185', 'VNOW-6184', 'VNOW-6176', 'VNOW-6175', 'VNOW-6172', 'VNOW-6151', 'VNOW-6150', 'VNOW-6146', 'VNOW-6144', 'VNOW-6137', 'VNOW-6133', 'VNOW-6128', 'VNOW-6124', 'VNOW-6122', 'VNOW-6115', 'VNOW-6113', 'VNOW-6107', 'VNOW-6096', 'VNOW-6094', 'VNOW-6093', 'VNOW-6092', 'VNOW-6087', 'VNOW-6082', 'VNOW-6081', 'VNOW-6078', 'VNOW-6076', 'VNOW-6074', 'VNOW-6072', 'VNOW-6042', 'VNOW-6024', 'VNOW-6020', 'VNOW-5992', 'VNOW-5974', 'VNOW-5971', 'VNOW-5961', 'VNOW-5932', 'VNOW-5928', 'VNOW-5914', 'VNOW-5900', 'VNOW-5891', 'VNOW-5890', 'VNOW-5879', 'VNOW-5868', 'VNOW-5867', 'VNOW-5843', 'VNOW-5825', 'VNOW-5804', 'VNOW-5756', 'VNOW-5739', 'VNOW-5733', 'VNOW-5696', 'VNOW-5691', 'VNOW-5643', 'VNOW-5561', 'VNOW-5527', 'VNOW-5492', 'VNOW-5483', 'VNOW-5476', 'VNOW-5452', 'VNOW-5423', 'VNOW-5422', 'VNOW-5399', 'VNOW-5366', 'VNOW-5365', 'VNOW-5340', 'VNOW-5319', 'VNOW-5303', 'VNOW-5293', 'VNOW-5290', 'VNOW-5239', 'VNOW-5224', 'VNOW-5200', 'VNOW-5184', 'VNOW-5160', 'VNOW-5108', 'VNOW-5104', 'VNOW-5072', 'VNOW-5016', 'VNOW-4993', 'VNOW-4989', 'VNOW-4976', 'VNOW-4905', 'VNOW-4903', 'VNOW-4901', 'VNOW-4900', 'VNOW-4874', 'VNOW-4873', 'VNOW-4870', 'VNOW-4864', 'VNOW-4840', 'VNOW-4835', 'VNOW-4834', 'VNOW-4830', 'VNOW-4817', 'VNOW-4809', 'VNOW-4808', 'VNOW-4807', 'VNOW-4806', 'VNOW-4758', 'VNOW-4742', 'VNOW-4725', 'VNOW-4707', 'VNOW-4577', 'VNOW-4547', 'VNOW-4250', 'VNOW-3870', 'VNOW-3759', 'VNOW-3523', 'VNOW-2903']
   const issueKey = Math.floor(Math.random() * bugs.length)
-  outputMessage(msg, bugs[issueKey], 'Here\'s a random Low priority bug from the Spark backlog')
+  outputMessage(msg, bugs[issueKey], "Here's a random Low priority bug from the Spark backlog")
 })
 
 // Respond to a JIRA issue (e.g. PX-1234)
@@ -92,7 +92,7 @@ function outputMessage (msg, issueKey, introText) {
       text: introText,
       attachments: [{
         text: '',
-        title: issueKey + ': ' + jiraIssue.fields.summary,
+        title: jiraIssue.fields.issuetype.name + ' ' + issueKey + ': ' + jiraIssue.fields.summary,
         thumb_url: avatarUrl,
         title_link: 'https://inmotionnow.atlassian.net/browse/' + issueKey,
         mrkdwn_in: ['fields'],
@@ -102,19 +102,14 @@ function outputMessage (msg, issueKey, introText) {
             'value': '`' + jiraIssue.fields.status.name + '`',
             'short': true
           },
-          {
-            'title': 'Assignee',
-            'value': jiraIssue.fields.assignee === null ? 'Unassigned' : jiraIssue.fields.assignee.displayName,
-            'short': true
-          },
+          // {
+          //   'title': 'Assignee',
+          //   'value': jiraIssue.fields.assignee === null ? 'Unassigned' : jiraIssue.fields.assignee.displayName,
+          //   'short': true
+          // },
           {
             'title': 'Priority',
             'value': '`' + jiraIssue.fields.priority.name + '`',
-            'short': true
-          },
-          {
-            'title': 'Type',
-            'value': jiraIssue.fields.issuetype.name,
             'short': true
           }
         ],
