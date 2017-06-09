d82957aa66f174fee38ba8e3394d9ceb6abefa7a
Darren Dunne <darrenwdunne@gmail.com>
2017-06-08 20:33:21 -0400

    customer

--- server.js
+++ server.js
@@ -179,6 +179,52 @@ function getColor (issuetype, priority) {
 
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
+  .message('feature', ['direct_mention', 'direct_message'], (msg) => {
+    msg
+      .say(`You want to open a feature? Who is the customer?`)
+      // sends next event from user to this route, passing along state
+      .route('customer', { greeting: text })
+  })
+  .route('customer', (msg, state) => {
+    var text = (msg.body.event && msg.body.event.text) || ''
+
+    // user may not have typed text as their next action, ask again and re-route
+    if (!text) {
+      return msg
+        .say("Whoops, I'm still waiting to hear how you're doing.")
+        .say('How are you?')
+        .route('customer', state)
+    }
+
+    // add their response to state
+    state.status = text
+
+    msg
+      .say(`Ok then. What's your favorite color?`)
+      .route('color', state)
+  })
+  .route('color', (msg, state) => {
+    var text = (msg.body.event && msg.body.event.text) || ''
+
+    // user may not have typed text as their next action, ask again and re-route
+    if (!text) {
+      return msg
+        .say("I'm eagerly awaiting to hear your favorite color.")
+        .route('color', state)
+    }
+
+    // add their response to state
+    state.color = text
+
+    msg
+      .say('Thanks for sharing.')
+      .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
+  // At this point, since we don't route anywhere, the "conversation" is over
+  })
+
+
+// "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
+slapp
   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
     msg
       .say(`${text}, how are you?`)
